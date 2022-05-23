70735b885a4572d60adf9be6e53d3214d38ba4dc
2297thanhtungf <2297thanhtungf@gmail.com>
2022-05-23 17:17:40 +0700

    change base

--- test_module/lib/main.dart
+++ test_module/lib/main.dart
@@ -8,23 +8,26 @@ import 'package:test_module/ultis/flavor_config.dart';
 import 'di.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
-  await FlavorConfig.instance.init(Flavors.prod);
-  //TODO get Header
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await EasyLocalization.ensureInitialized();
+  // await FlavorConfig.instance.init(Flavors.prod);
+  // //TODO get Header
   setupLocator(flavorConfig: FlavorConfig.instance);
-  // setPathUrlStrategy();
+  // // setPathUrlStrategy();
 
-  EasyLoading.instance
-    ..loadingStyle = EasyLoadingStyle.light
-    ..radius = 10.0
-    ..maskColor = Color(0xff231F20).withOpacity(0.5)
-    ..userInteractions = false
-    ..dismissOnTap = false;
+  // EasyLoading.instance
+  //   ..loadingStyle = EasyLoadingStyle.light
+  //   ..radius = 10.0
+  //   ..maskColor = Color(0xff231F20).withOpacity(0.5)
+  //   ..userInteractions = false
+  //   ..dismissOnTap = false;
 
-  runApp(EasyLocalization(
-      supportedLocales: const [AppLocales.vi],
-      path: 'AppPaths.assetTranslationsPath',
-      fallbackLocale: AppLocales.vi,
-      child:  chooseWidget('') ));
+    return runApp((RootApp()));
+
+
+  // runApp(EasyLocalization(
+  //     supportedLocales: const [AppLocales.vi],
+  //     path: 'AppPaths.assetTranslationsPath',
+  //     fallbackLocale: AppLocales.vi,
+  //     child:  chooseWidget('') ));
 }
